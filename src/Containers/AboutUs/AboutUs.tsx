6469edc1bae56809a122482d0266a18a77ed617f
Arsen Chernov <arsenchernov02@gmail.com>
2024-02-10 03:36:38 +0600

    Added some info in About Us and Contacts

--- src/Containers/AboutUs/AboutUs.tsx
+++ src/Containers/AboutUs/AboutUs.tsx
@@ -1,7 +1,15 @@
 const AboutUs = () => {
     return (
         <div>
-            something about us
+            <p className="fs-2">Hi there! My name is Arsen, and this is my personal blog.</p>
+            <p>I'm a student at KIRGIZİSTAN-TÜRKİYE MANAS ÜNİVERSİTESİ, faculty of Tourism.</p>
+            <ul>My hobbies are:
+                <li>Working out</li>
+                <li>Playing Video Games</li>
+                <li>Skiing</li>
+                <li>Cooking</li>
+            </ul>
+            <p>I will try to write(no) new posts as often as possible :D</p>
         </div>
     );
 };
--- src/Containers/Contacts/Contacts.tsx
+++ src/Containers/Contacts/Contacts.tsx
@@ -1,7 +1,11 @@
 const Contacts = () => {
     return (
         <div>
-            some contacts
+            <p className="fs-2">
+                If you want to contact me for some business proposals, or just to say 'Hi!', here are my contacts:
+            </p>
+            <span className="fs-4 d-block">Phone Number: 0551 241 102</span>
+            <span className="fs-4 d-block">Email: arsenchernov02@gmail.com</span>
         </div>
     );
 };
