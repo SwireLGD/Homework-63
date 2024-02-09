e9eafae2929b4f77a9df0663e13aa02806ab55ab
Arsen Chernov <arsenchernov02@gmail.com>
2024-02-10 03:46:18 +0600

    Added try catch and loader in posts.tsx

--- src/Containers/Posts/Posts.tsx
+++ src/Containers/Posts/Posts.tsx
@@ -3,27 +3,39 @@ import { ApiPosts, Post } from "../../types";
 import axiosApi from "../../axiosApi";
 import { Link } from "react-router-dom";
 import {format} from "date-fns";
+import Loader from '../../Components/Loader/Loader';
 
 const Posts = () => {
     const [posts, setPosts] = useState<Post[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchPosts = useCallback(async () => {
-        const response = await axiosApi.get<ApiPosts | null>('/posts.json');
-        const posts = response.data;
+        try {
+            const response = await axiosApi.get<ApiPosts | null>('/posts.json');
+            const posts = response.data;
 
-        if (posts) {
-            setPosts(Object.keys(posts).map(id => ({
-                ...posts[id],
-                id
-            })));
-        } else {
-            setPosts([]);
+            if (posts) {
+                setPosts(Object.keys(posts).map(id => ({
+                    ...posts[id],
+                    id
+                })));
+            } else {
+                setPosts([]);
+            }
+        } catch (error) {
+            console.error('An error occurred while fetching the posts', error);           
+        } finally {
+            setIsLoading(false);
         }
     }, []);
 
-    useEffect(() => {
+    useEffect(() => { 
         void fetchPosts();
     }, [fetchPosts]);
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return (
         <div className="mt-3 d-flex flex-column gap-3">
